013a08ba463e612b87cb25f97350fcf50838bd28
Amr Zakaria <amr3zakarya@gmail.com>
2025-08-29 21:04:56 -0700

    Created the project and ad some main components

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import HomeCards from './components/HomeCards'
+import JobListing from './components/JobListing'
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <Hero />
+      <HomeCards/>
+      <JobListing/>
+
+    {/* <!-- Browse Jobs --> */}
+    
+
+    <section className="m-auto max-w-lg my-10 px-6">
+      <a
+        href="jobs.html"
+        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >View All Jobs</a
+      >
+    </section>
+    </>
+  )
+}
+
+export default App
