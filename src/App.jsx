78bf66cc955ebb521dbb6af5f5f1a884c67efa28
Amr Zakaria <amr3zakarya@gmail.com>
2025-08-31 21:32:02 -0700

    NavLink and 404 page  done

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } 
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/homePage';
 import JobsPage from './pages/JobsPage';
+import NotFoundPage from './pages/NotFoundPage';
+
 
 const route = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />} >
       <Route index element={<HomePage />} />
       <Route path='/jobs' element={<JobsPage/>} />
+      <Route path='*' element={<NotFoundPage/>} />
     </Route>
   )
 )
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,7 +9,7 @@ function HomePage() {
         <>
             <Hero />
             <HomeCards />
-            <JobListing />
+            <JobListing isHome='true' />
             <ViewAllJobs />
         </>
     )
