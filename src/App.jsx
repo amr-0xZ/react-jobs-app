0d687e42cb602084f632a953cb880ca548d5ce0a
Amr Zakaria <amr3zakarya@gmail.com>
2025-08-31 18:09:49 -0700

    jobs page done and add the Link thing so no more refreshing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 import React from 'react';
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/homePage';
+import JobsPage from './pages/JobsPage';
 
 const route = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />} >
       <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobsPage/>} />
     </Route>
   )
 )
