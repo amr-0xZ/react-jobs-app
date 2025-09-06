582a8bc15d2e5b05ff9adbbb47064349625fd2cc
Amr Zakaria <amr3zakarya@gmail.com>
2025-09-05 23:36:19 -0700

    theme file and logo added.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import logo from '../assets/images/logo.png'
+import logo from '../assets/images/jobss.png'
 
 function Navbar() {
 
